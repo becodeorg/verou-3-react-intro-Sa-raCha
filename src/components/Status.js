87d12b67f2fccf5c2aca133f4326750e30c46f11
Sa-raCha <sarabibimagdalena@gmail.com>
2022-04-14 16:16:58 +0200

    Count todo's on the page

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,5 +1,17 @@
-const Status = () => {
+import { useContext } from "react";
+import { TodolistContext } from "../contexts/todolist";
 
+const Status = () => {
+    const [state, dispatch] = useContext(TodolistContext);
+    const todoCount = state.todos.filter((todo) => !todo.isCompleted).length;
+    
+    return (
+        <footer>
+            <span className="todo-count">
+                {todoCount}
+            </span>
+        </footer>
+    );
 };
 
 export default Status;
