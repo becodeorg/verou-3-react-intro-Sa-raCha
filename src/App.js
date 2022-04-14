de6032801007d67b2bcdc740eb3b5a562fb2c96f
Sa-raCha <sarabibimagdalena@gmail.com>
2022-04-14 15:57:24 +0200

    Create todo status component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import List from "./components/List";
+import Status from "./components/Status";
 import { TodolistProvider } from "./contexts/todolist";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Header/>
         <List/>
+        <Status/>
       </div>
     </TodolistProvider>
   );
